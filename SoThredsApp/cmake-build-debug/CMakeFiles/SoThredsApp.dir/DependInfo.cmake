222ea21c1b920cb8d677046341f57fbb93bfaa5c
Golec5000 <danielryszkowski@gmail.com>
2024-03-31 00:05:35 +0100

    update

---
+++ SoThredsApp/cmake-build-debug/CMakeFiles/SoThredsApp.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wombat/GitHub/SO-project/SoThredsApp/main/main.cpp" "CMakeFiles/SoThredsApp.dir/main/main.cpp.o" "gcc" "CMakeFiles/SoThredsApp.dir/main/main.cpp.o.d"
+  "/home/wombat/GitHub/SO-project/SoThredsApp/map/MainAppMap.cpp" "CMakeFiles/SoThredsApp.dir/map/MainAppMap.cpp.o" "gcc" "CMakeFiles/SoThredsApp.dir/map/MainAppMap.cpp.o.d"
+  "/home/wombat/GitHub/SO-project/SoThredsApp/threads/entity/Person.cpp" "CMakeFiles/SoThredsApp.dir/threads/entity/Person.cpp.o" "gcc" "CMakeFiles/SoThredsApp.dir/threads/entity/Person.cpp.o.d"
+  "/home/wombat/GitHub/SO-project/SoThredsApp/threads/entityGenerator/EntityGenerator.cpp" "CMakeFiles/SoThredsApp.dir/threads/entityGenerator/EntityGenerator.cpp.o" "gcc" "CMakeFiles/SoThredsApp.dir/threads/entityGenerator/EntityGenerator.cpp.o.d"
+  "/home/wombat/GitHub/SO-project/SoThredsApp/threads/mapRender/MapRender.cpp" "CMakeFiles/SoThredsApp.dir/threads/mapRender/MapRender.cpp.o" "gcc" "CMakeFiles/SoThredsApp.dir/threads/mapRender/MapRender.cpp.o.d"
+  "/home/wombat/GitHub/SO-project/SoThredsApp/threads/switch/SwitchThread.cpp" "CMakeFiles/SoThredsApp.dir/threads/switch/SwitchThread.cpp.o" "gcc" "CMakeFiles/SoThredsApp.dir/threads/switch/SwitchThread.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
